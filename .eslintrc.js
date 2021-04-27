0121ea6f845c10f2713fbed41168151b2e087520
dvdrtrgn <dvdrtrgn@gmail.com>
2021-04-27 14:01:15 -0500

    Try youtube embed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: [
-    'vue',
-  ],
+  plugins: ['vue'],
   rules: {
     'no-console': 'off',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'vue/comment-directive': 'off',
     'no-irregular-whitespace': 'off',
+    'max-len': ['error', { code: 120 }],
   },
 };
